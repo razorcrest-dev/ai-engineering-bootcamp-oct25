fcee8f99ceb1004513f2d1fee88480f549e26e18
razor-crest <dush792@gmail.com>
2025-10-21 13:04:08 +0530

    Added basic observability using Langsmith

--- src/api/core/config.py
+++ src/api/core/config.py
@@ -4,6 +4,10 @@ class Config(BaseSettings):
     OPENAI_API_KEY: str
     GROQ_API_KEY: str
     GOOGLE_API_KEY: str
+    # LANGSMITH_API_KEY: str
+    # LANGSMITH_TRACING: bool = True
+    # LANGSMITH_ENDPOINT: str = "https://api/smith.langchain.com"
+    # LANGSMITH_PROJECT: str = "rag-tracing"
 
     model_config = SettingsConfigDict(env_file=".env")
 
